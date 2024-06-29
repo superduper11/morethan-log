4810e6176041ed415cc990ea997dfda1c929482e
kingdonut <95068411+superduper11@users.noreply.github.com>
2024-06-30 09:06:53 +1000

    Update _document.tsx
    Adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ class MyDocument extends Document {
     return (
       <Html lang={CONFIG.lang}>
         <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9579104518546678"
+           crossorigin="anonymous"></script>
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
